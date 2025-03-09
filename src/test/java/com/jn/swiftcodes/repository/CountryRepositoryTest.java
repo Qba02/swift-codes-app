973b1aae02fdb2630ecd1ef50e3b801559e3e17c
Jakub Nowak <njakub002@gmail.com>
2025-03-09 17:28:54 +0100

    Test classes added

---
+++ src/test/java/com/jn/swiftcodes/repository/CountryRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.jn.swiftcodes.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountryRepositoryTest {
+
+}
---
+++ src/test/java/com/jn/swiftcodes/service/BankServiceTest.java
@@ -0,0 +1,7 @@
+package com.jn.swiftcodes.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankServiceTest {
+
+}
