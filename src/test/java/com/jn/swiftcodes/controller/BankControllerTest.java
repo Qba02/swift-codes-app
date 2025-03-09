7b2d099fd09094f9c5c498801a74221920edd359
Jakub Nowak <njakub002@gmail.com>
2025-03-09 19:42:50 +0100

    Testing environment configured

--- src/test/java/com/jn/swiftcodes/controller/BankControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.jn.swiftcodes.controller;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class BankControllerTest {
-
-}
--- src/test/java/com/jn/swiftcodes/repository/CountryRepositoryTest.java
+++
@@ -1,7 +0,0 @@
-package com.jn.swiftcodes.repository;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class CountryRepositoryTest {
-
-}
